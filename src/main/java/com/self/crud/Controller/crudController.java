b18184e57ec64316557872291b3738a8b46422dd
Mohd Waseem <mohd.waseem@comviva.com>
2025-10-23 16:35:17 +0530

    commiting chnages for Swagger API documentation

--- src/main/java/com/self/crud/Controller/crudController.java
+++ src/main/java/com/self/crud/Controller/crudController.java
@@ -5,6 +5,9 @@ import com.self.crud.Service.EmployeeService;
 import com.self.crud.exception.ErrorDetails;
 import com.self.crud.exception.ResourceNotFoundException;
 import com.self.crud.model.Employee;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +18,10 @@ import org.springframework.web.context.request.WebRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Tag(
+        name = "Employee Management System",
+        description = "APIs for managing employees (Create, update, get, delete)"
+)
 @RestController
 @RequestMapping("/employees")
 public class crudController {
@@ -24,12 +31,29 @@ public class crudController {
         this.employeeService = employeeService;
     }
 
+    @Operation(
+            summary = "Get all Employees",
+            description = "Retrieve a list of all employees from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successfully retrieved list of employees"
+    )
+
     @GetMapping("/listAll")
     public ResponseEntity<List <EmployeeDto>> getAllEmployees(){
         List <EmployeeDto> listEmployee = employeeService.getAllEmployees();
         return new ResponseEntity<>(listEmployee, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Create Employee Rest API",
+            description = "Save a employees to the database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Employee created successfully"
+    )
     @PostMapping("/create")
     //@ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<EmployeeDto> saveEmployee(@Valid @RequestBody EmployeeDto employee){
@@ -37,6 +61,14 @@ public class crudController {
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get employee by ID",
+            description = "Retrieve a specific employee using their unique ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successfully retrieved the employee"
+    )
     @GetMapping("/list/{id}")
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Integer empId){
         EmployeeDto employee = employeeService.getEmployeeById(empId);
@@ -46,15 +78,40 @@ public class crudController {
         //return ResponseEntity.OK().header("Custom-Header", "CustomValue").body(employeeService.getEmployeeById(id));
     }
 
+    @Operation(
+            summary = "Update employee by ID",
+            description = "Update a specific employee using their unique ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successfully updated the employee"
+    )
     @PutMapping("/update/{id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@Valid @PathVariable Integer id, @RequestBody EmployeeDto employee){
         return ResponseEntity.ok(employeeService.updateEmployee(employee,id));
     }
 
+    @Operation(
+            summary = "Partial update employee by ID",
+            description = "Partially update a specific employee using their unique ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successfully updated the employee"
+    )
     @PatchMapping("/partialUpdate/{id}")
     public ResponseEntity<EmployeeDto> partialUpdateEmployee(@PathVariable Integer id, @RequestBody EmployeeDto employee){
         return ResponseEntity.ok(employeeService.partialUpdateEmployee(employee,id));
     }
+
+    @Operation(
+            summary = "Delete employee by ID",
+            description = "Delete a specific employee using their unique ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successfully deleted the employee from the database"
+    )
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteEmployeeById(@PathVariable Integer id){
         employeeService.deleteEmployeeById(id);
--- src/main/java/com/self/crud/DTO/EmployeeDto.java
+++ src/main/java/com/self/crud/DTO/EmployeeDto.java
@@ -1,5 +1,6 @@
 package com.self.crud.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -8,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "EmployeeDTO Model Information")
 @Setter
 @Getter
 @NoArgsConstructor
@@ -15,13 +17,16 @@ import lombok.Setter;
 public class EmployeeDto {
     private int id;
 
+    @Schema(description = "Employee Name", example = "John Doe")
     @NotEmpty(message = "Employee name should not be empty or null")  //if message is not given, default message will be shown(must not be empty)
     private String name;
 
+    @Schema(description = "Employee EmailAddress", example = "xyz@hotmail.com")
     @NotEmpty(message = "Employee email should not be empty or null")
     @Email
     private String email;
 
+    @Schema(description = "Employee Salary")
     @NotNull(message = "Employee salary should not be null")
     private Long salary;
 
